ec08863b79e3f7f7ac7a345180f40b4172291615
Marlon <marlon.engels@student.hogent.be>
2023-03-04 22:58:05 +0100

    TUI scrape updated

--- webscraping_scripts/TUI_Scrape.py
+++ webscraping_scripts/TUI_Scrape.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
-from datetime import datetime
-from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import NoSuchElementException  
+from datetime import date
+from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 # from webdriver_manager.chrome import ChromeDriverManager 
-import json, requests, re, datetime, csv
+import json, re, csv, pandas as pd
+
+def init_csv(today):
+    COLUMNS=["flightNumber", "departureAirportCode", "departureAirportName", "departureCountryCode", "departureDate", "departureTime", "arrivalAirportCode", "arrivalAirportName", "arrivalCountryCode", "arrivalDate", "arrivalTime", "adultPrice", "originalAdultPrice", "journeyType", "journeyDuration", "totalNumberOfStops", "carrierCode", "carrierName", "availableSeats", "isCheapest", "airportTax", "originalAirportTax", "bookingFee", "originalBookingFee", "isOriginalBooking"]
+    url = f'data/tuifly/tuiFlyScrapeData_{today}.csv'
+    with open(url, 'w', newline='') as file: 
+        writer = csv.writer(file)
+        writer.writerow(COLUMNS)
+
+def data_to_csv(data, today):
+    url = f'data/tuifly/tuiFlyScrapeData_{today}.csv'
+    with open(url, 'a', newline='') as file: 
+        writer = csv.writer(file)
+        writer.writerow(data)
 
 def get_raw_data(url):
         PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
@@ -23,66 +34,87 @@ def get_raw_data(url):
         driver.find_element(By.CSS_SELECTOR, "#cmCloseBanner").click()
 
         WebDriverWait(driver, 50).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container footer > script"))
+                EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container > :nth-child(3)"))
         )
 
-        result = driver.find_element(By.CSS_SELECTOR, "div#page div.container footer > script").get_attribute('innerHTML')
+        result = driver.find_element(By.CSS_SELECTOR, "div#page div.container > :nth-child(3)").get_attribute('innerHTML')
         driver.close()
-        if result is None:
-            return
-        match = re.search(r'window\.dataLayer\.push\(({.*})\);', result)
-
+        match = re.search(r"var searchResultsJson = ({.*});", result)
         if match:
             json_data = match.group(1)
             return json.loads(json_data)
 
-def get_dates():
-    dates = []
-    for month in range(4, 11):
-        for day in range(1, 32):
-            try:
-                date = datetime.date(2023, month, day)
-                if date >= datetime.date(2023, 4, 1) and date <= datetime.date(2023, 10, 1):
-                    dates.append(date.strftime("%Y-%m-%d"))
-            except ValueError:
-                pass
-    return dates
-
-def init_csv():
-    COLUMNS=['origin','destination','departtime','arrivaltime','duration','price','daysToDeparture','directOrIndirectFlight']
-    with open('data/tuiFlyScrapeData.csv', 'w', newline='') as file: 
-        writer = csv.writer(file)
-        writer.writerow(COLUMNS)
-
-def get_useful_data(raw_data, date):
-    departureAirportCodes = raw_data["page"]["search"]["departureAirportCodes"]
-    arrivalAirportCodes = raw_data["page"]["search"]["arrivalAirportCodes"]
-    for flight in raw_data["page"]["search"]["resultList"]["outboundFlights"]:
-        if flight["departureDate"] == date:
-            departureDate = flight["departureDate"] + "T" + flight["departureTime"] + ":00:000"
-            arrivalDate = flight["arrivalDate"] + "T" + flight["arrivalTime"] + ":00:000"
-            duration_str = flight["flightDuration"]
-            hours, minutes = int(duration_str.split("u ")[0]), int(duration_str.split("u ")[1].replace("m", ""))
-            duration_formatted = f"{hours:02}:{minutes:02}"
-            price = flight["price"]["currentPrice"]
-            directOrIndirectFlight = flight["directOrIndirectFlight"]
-            daysToDeparture = flight["daysToDeparture"]
-            data_to_csv([departureAirportCodes,arrivalAirportCodes,departureDate,arrivalDate,duration_formatted,price,daysToDeparture,directOrIndirectFlight])
-
-def data_to_csv(data):
-    with open('data/tuiFlyScrapeData.csv', 'a', newline='') as file: 
-        writer = csv.writer(file)
-        writer.writerow(data)
+def get_useful_data(raw_data, date, today):
+    if len(raw_data["flightViewData"]) > 0:
+        departureCountryCode = raw_data["depAirportData"][0]["countryCode"]
+        arrivalCountryCode = raw_data["arrAirportData"][0]["countryCode"]
+        for flight in raw_data["flightViewData"]:
+            if flight["departureDate"] == date:
+                flightNumber = flight['flightsectors'][0]['flightNumber']
+                departureAirportCode = flight["journeySummary"]["departAirportCode"]
+                departureAirportName = flight["flightsectors"][0]["departureAirport"]["name"]
+                departureDate = flight["departureDate"]
+                departureTime = flight["flightsectors"][0]["schedule"]["depTime"]
+                arrivalAirportCode = flight["journeySummary"]["arrivalAirportCode"]
+                arrivalAirportName = flight["flightsectors"][0]["arrivalAirport"]["name"]
+                arrivalDate = flight['journeySummary']["arrivalDate"]
+                arrivalTime = flight["flightsectors"][0]["schedule"]["arrTime"]
+                adultPrice = flight["adultPrice"]
+                originalAdultPrice = flight["originalAdultPrice"]
+                journeyType = flight["journeySummary"]["journeyType"]
+                journeyDuration = flight["journeySummary"]["journeyDuration"]
+                hours, minutes = int(journeyDuration.split("u ")[0]), int(journeyDuration.split("u ")[1].replace("m", ""))
+                journeyDuration = f"{hours:02}:{minutes:02}"
+                totalNumberOfStops = flight["journeySummary"]["totalNumberOfStops"]
+                carrierCode = flight["journeySummary"]["carrierCode"]
+                carrierName = flight["journeySummary"]["carrierName"]
+                availableSeats = flight["journeySummary"]["availableSeats"]
+                isCheapest = flight["isCheapest"]
+                airportTax = flight["airportTax"]
+                originalAirportTax = flight["originalAirportTax"]
+                bookingFee = flight["bookingFee"]
+                originalBookingFee = flight["originalBookingFee"]
+                isOriginalBooking = flight["isOriginalBooking"]
+                data_to_csv([
+                    flightNumber,
+                    departureAirportCode,
+                    departureAirportName,
+                    departureCountryCode,
+                    departureDate,
+                    departureTime,
+                    arrivalAirportCode,
+                    arrivalAirportName,
+                    arrivalCountryCode,
+                    arrivalDate,
+                    arrivalTime,
+                    adultPrice,
+                    originalAdultPrice,
+                    journeyType,
+                    journeyDuration,
+                    totalNumberOfStops,
+                    carrierCode,
+                    carrierName,
+                    availableSeats,
+                    isCheapest,
+                    airportTax,
+                    originalAirportTax,
+                    bookingFee,
+                    originalBookingFee,
+                    isOriginalBooking
+                    ], today)
 
 def start():
-    init_csv()
+    today = date.today()
+    today = today.strftime("%Y-%m-%d")
+    init_csv(today)
     ORIGIN = ['BRU', 'OST', 'ANR', 'LGG']
     DESTINATION = ['CFU','HER','RHO','BDS','NAP','PMO','FAO','ALC','IBZ','AGP','SPC','TFS']
-    for date in get_dates():
+    for depDate in pd.date_range('2023-04-01','2023-10-31'):
         for origin in ORIGIN:
             for destination in DESTINATION:
-                url = f'http://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D={origin}&flyingTo%5B%5D={destination}&depDate={date}&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=false&currency=EUR&isOneWay=true'
+                url = f'http://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D={origin}&flyingTo%5B%5D={destination}&depDate={depDate.strftime("%Y-%m-%d")}&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=false&currency=EUR&isOneWay=true'
                 raw_data = get_raw_data(url)
-                get_useful_data(raw_data, date)
+                if raw_data:
+                    get_useful_data(raw_data, depDate.strftime("%Y-%m-%d"), today)
 
 start()
