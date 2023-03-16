0857e5308fc63fe2c8ccc474d2fdcdf6ea0e813f
FLORIS BUYSE <floris.buyse@student.hogent.be>
2023-03-16 19:01:44 +0100

    bruair opnieuw begonnen in file test2.py, bijna af (nog 2 todo items)

--- webscraping_scripts/test2.py
+++ webscraping_scripts/test2.py
@@ -1,42 +1,32 @@
 from seleniumwire import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import re
-import time
-import csv
-from datetime import date
-from fake_useragent import UserAgent
 from selenium_stealth import stealth
+from datetime import date
+import time
 import gzip
 import io
-import datetime
 import json
-
-PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+import csv
 
 DESTINATION_ARRAY = ["kerkyra", "heraklion", "rhodes", "brindisi", "napels", "palermo", "faro", "alicante", "ibiza",
                      "malaga", "palma-de-mallorca", "tenerife"]
-
 HEADERS = ['scrapeDate', 'departureAirportCode', 'departureAirportName', 'departureCountryCode', 'arrivalAirportCode', 'arrivalAirportName', 'arrivalCountryCode',
            'duration', 'aantalTussenstops', 'availableSeats', 'flightNumber', 'carrierCode', 'carrierName', 'departureDate', 'departureTime', 'arrivalDate', 'arrivalTime', 'totalPrice', 'taxIncludedInPrice']
 
-# Instantiate a UserAgent object
-user_agent = UserAgent()
-
-# Driver
-selenium_wire_options = {
-    'verify_ssl': False,
-    'accept_insecure_certs': True,
-}
-
+# driver options
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument(f'user-agent={user_agent.chrome}')
-options.add_experimental_option("detach", True)
 options.add_argument('--ignore-certificate-errors')
-driver_service = Service(executable_path=PATH)
-driver = webdriver.Chrome(service=driver_service,
-                          options=options, seleniumwire_options=selenium_wire_options)
+options.add_argument('--ignore-ssl-errors')
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--disable-application-cache')
+options.add_argument('--disable-cache')
+
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.implicitly_wait(25)
+
 stealth(driver,
         languages=["nl-NL", "nl"],
         vendor="Google Inc.",
@@ -45,58 +35,50 @@ stealth(driver,
         renderer="Intel Iris OpenGL Engine",
         fix_hairline=True,
         )
-driver.maximize_window()
-driver.implicitly_wait(25)
+
+# URL's joinen, cookies accepteren, datum / vlucht selecteren
 
 
-def driver_init(datecsv, dest):
+def driver_init(dest):
+    # URL's
     url = f"https://www.brusselsairlines.com/lhg/be/nl/o-d/cy-cy/brussel-{dest}"
-
     driver.get("https://www.google.com")
     time.sleep(2)
     driver.get(url)
-    # EERSTE PAGINA, hierna wordt datum veranderd bij de pagina met prijzen
+
     # Cookie accept
     try:
         driver.find_element(By.ID, "cm-acceptAll").click()
     except:
         pass
-
     time.sleep(2)
-
-    # Doorgaan drukken
     driver.find_element(By.CLASS_NAME, "active-hidden").click()
-
     time.sleep(2)
 
-    # Enkele vluchtaanduiden
+    # Vlucht kiezen en datum selecteren
     driver.find_element(
         By.XPATH, "//label[contains(text(), 'Alleen enkele vlucht')]").click()
-
     time.sleep(2)
-
-    # Datum selecteren
     driver.find_element(By.ID, "departureDate").click()
     time.sleep(2)
     driver.find_element(By.CLASS_NAME, "move-forward").click()
     time.sleep(2)
     driver.find_element(
         By.CSS_SELECTOR, '[data-date="1"][data-month="3"][data-year="2023"]').click()
-
     time.sleep(2)
-
-    # "Vlucht zoeken" drukken
     driver.find_element(By.ID, "searchFlights").click()
-
-    # Nework request opvangen
     time.sleep(10)
-    for i in driver.requests:
-        if "air-bounds" in i.path:
-            body = i.response.body
+
+# Data ophalen uit network requests
+
+
+def get_data(datecsv):
+    for request in driver.requests:
+        if request.response and "air-bounds" in request.path:
+            body = request.response.body
             body_str = gzip.GzipFile(
                 fileobj=io.BytesIO(body)).read().decode('utf-8')
             body_dict = json.loads(body_str)
-            print(body_dict)
 
             data = body_dict["data"]["airBoundGroups"]
             dictLocatie = body_dict["dictionaries"]["location"]
@@ -104,7 +86,6 @@ def driver_init(datecsv, dest):
             dictFlight = body_dict["dictionaries"]["flight"]
 
             for vlucht in data:
-                # print(vlucht)
                 departureAirportCode = vlucht["boundDetails"]["originLocationCode"]
                 departureAirportName = dictLocatie[departureAirportCode]["airportName"]
                 departureCountryCode = dictLocatie[departureAirportCode]["countryCode"]
@@ -119,9 +100,6 @@ def driver_init(datecsv, dest):
                 aantalTussenstops = len(vlucht["boundDetails"]["segments"]) - 1
                 availableSeats = vlucht["airBounds"][0]["availabilityDetails"][0]["quota"]
                 departureFlightId = vlucht["boundDetails"]["segments"][0]["flightId"]
-                # print(departureFlightId)
-                print(dictFlight[departureFlightId])
-                print()
 
                 if "operatingAirlineName" in dictFlight[departureFlightId]:
                     carrierCode = 'SN'
@@ -138,12 +116,6 @@ def driver_init(datecsv, dest):
                 arrivalDate, arrivalTime = dictFlight[arrivalflightId]["arrival"]["dateTime"].split(
                     "T")
 
-                # if carrierCode == 'CITYJET FOR BRUSSELS AIRLINES':
-                #     carrierName = 'CITYJET FOR BRUSSELS AIRLINES'
-                # else:
-                #     carrierName = dictAirline[carrierCode]
-                # departureDate = datetime.datetime.strptime(
-                #     flightId[-15:], "%Y-%m-%d-%H%M").strftime("%Y-%m-%dT%H:%M:%S")
                 totalPrice = vlucht["airBounds"][0]["prices"]["totalPrices"][0]["total"] / 100
                 taxIncludedInPrice = vlucht["airBounds"][0]["prices"]["totalPrices"][0]["totalTaxes"] / 100
 
@@ -153,20 +125,17 @@ def driver_init(datecsv, dest):
 
 
 def veranderDatum():
-    tabList = driver.find_element(By.CLASS_NAME, "tabList")
-    items = tabList.find_elements(By.CLASS_NAME, "calendarItem")
-    if items[4].get_attribute("class") == "calendarItem noAvail ng-star-inserted":
-        if items[5].get_attribute("class") == "calendarItem noAvail ng-star-inserted":
-            if items[6].get_attribute("class") == "calendarItem noAvail ng-star-inserted":
-                driver.find_element(
-                    By.CSS_SELECTOR, ".container.smile .label").click()
-                current = int(driver.find_element(
-                    By.CSS_SELECTOR, ".day.starting").get_attribute('innerHTML'))
-                current += 4
-                driver.find_element(
-                    By.CSS_SELECTOR, f".day.day{current}").click()
-                driver.find_element(
-                    By.CSS_SELECTOR, ".newSearchButton.secondary.ng-tns-c63-0").click()
+    container = driver.find_element(
+        By.CSS_SELECTOR, ".carousel:first-of-type")
+    items = container.find_elements(By.TAG_NAME, "button")
+    innerText4 = items[4].text
+    innerText5 = items[5].text
+    innerText6 = items[6].text
+    if innerText4 == "Geen tarieven" or innerText4 == "Uitverkocht":
+        if innerText5 == "Geen tarieven" or innerText5 == "Uitverkocht":
+            if innerText6 == "Geen tarieven" or innerText6 == "Uitverkocht":
+                # TODO: Als alle 3 geen tarieven zijn, dan moet er een nieuwe datum gekozen worden
+                pass
             else:
                 items[6].click()
         else:
@@ -182,8 +151,6 @@ def init_csv(date):
         writer = csv.writer(file)
         writer.writerow(HEADERS)
 
-# append a row of data to the csv file
-
 
 def data_to_csv(data, date):
     url = f'./data/bruair/BruAirScrapeData_{date}.csv'
@@ -195,10 +162,16 @@ def data_to_csv(data, date):
 def start():
     today = date.today()
     init_csv(today)
-    driver_init(today, DESTINATION_ARRAY[9])
+    time.sleep(2)
+    get_data(today)
+    time.sleep(5)
     veranderDatum()
+    time.sleep(5)
+    driver.refresh()
+    time.sleep(7)
 
 
-start()
-
-# driver_init(DESTINATION_ARRAY[9])
+# TODO : Destination array loopen en range aanpassen
+driver_init(DESTINATION_ARRAY[9])
+for _ in range(3):
+    start()
