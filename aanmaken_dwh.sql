2b7414b59c32a3df44becc1ff0776d5aacb97dfe
Emma De Koster <emma.dekoster@student.hogent.be>
2023-05-04 08:45:59 +0200

    scripts mysql

--- aanmaken_dwh.sql
+++ aanmaken_dwh.sql
@@ -2,20 +2,9 @@ CREATE DATABASE airfaresDWH;
 
 use airfaresDWH;
 
-CREATE TABLE FactFlights (
-  carrierCode VARCHAR(50),
-  flightKey VARCHAR(255),
-  scrapeDate DATE,
-  airportCode VARCHAR(50),
-  flightNumber VARCHAR(50),
-  departureDate DATE,
-  arrivalDate DATE,
-  totalNumberOfStops INT,
-  PRIMARY KEY (flightKey, scrapeDate)
-);
 
 CREATE TABLE DimFlight (
-  flightKey VARCHAR(255) NOT NULL,
+  flightID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   flightNumber VARCHAR(50),
   departureDate DATE,
   arrivalDate DATE,
@@ -23,40 +12,27 @@ CREATE TABLE DimFlight (
   arrivalTime TIME,
   journeyDuration TIME,
   totalNumberOfStops INT,
-  carrierCode VARCHAR(50),
-  depAirportCode VARCHAR(50),
-  arrAirportCode VARCHAR(50),
   startDate DATE,
-  endDate DATE,
-  PRIMARY KEY (flightKey),
-  UNIQUE KEY (flightKey)
+  endDate DATE
 );
 
-CREATE TABLE DimPrice (
-  priceID INT AUTO_INCREMENT PRIMARY KEY,
-  flightKey VARCHAR(255),
-  scrapeDate DATE,
-  availableSeats INT,
-  adultPrice FLOAT,
-  FOREIGN KEY (flightKey) REFERENCES DimFlight(flightKey)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-);
 
 CREATE TABLE DimAirline (
+carrierID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 carrierCode VARCHAR(10),
 carrierName VARCHAR(50)
 );
 
 CREATE TABLE DimAirport (
-airportCode VARCHAR(10) PRIMARY KEY,
+airportID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+airportCode VARCHAR(10),
 airportName VARCHAR(50),
 city VARCHAR(50),
 country VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS DimDate  (
-    date_id INT NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS DimDate (
+    dateID INT UNSIGNED,
     fullDate DATE,
     dayOfMonth INT,
     dayOfYear INT,
@@ -66,6 +42,34 @@ CREATE TABLE IF NOT EXISTS DimDate  (
     monthName VARCHAR(10),
     year INT,
     isItWeekend INT,
-    isItVacationday INT,
-    PRIMARY KEY(date_id)
+    isItVacationDay INT,
+    PRIMARY KEY (dateID)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000;
+
+
+
+
+
+CREATE TABLE FactFlights (
+  flightKey VARCHAR(255),
+  flightNumber VARCHAR(50),
+  carrierCode VARCHAR(50),
+  scrapeDate DATE,
+  depAirportCode VARCHAR(50),
+  arrAirportCode VARCHAR(50),
+  flightID INT,
+  departureDate DATE,
+  arrivalDate DATE,
+  totalNumberOfStops INT,
+  availableSeats INT,
+  adultPrice FLOAT,
+  carrierID INT,
+  airportID INT,
+  dateID INT UNSIGNED,
+  PRIMARY KEY (flightKey),
+  FOREIGN KEY (flightID) REFERENCES DimFlight(flightID),
+  FOREIGN KEY (carrierID) REFERENCES DimAirline(carrierID),
+  FOREIGN KEY (airportID) REFERENCES DimAirport(airportID),
+  FOREIGN KEY (dateID) REFERENCES DimDate(dateID)
+  
+);
